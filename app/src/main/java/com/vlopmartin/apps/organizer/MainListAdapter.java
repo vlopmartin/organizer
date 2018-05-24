3db080c4c2d385a35d52cc73b65160cdfa9db9bf
Victor <vlopmartin@gmail.com>
2018-05-24 21:49:17 +0200

    Initial commit (testing)

---
+++ app/src/main/java/com/vlopmartin/apps/organizer/MainListAdapter.java
@@ -0,0 +1,51 @@
+package com.vlopmartin.apps.organizer;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<String> dataList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View item;
+        public ViewHolder(View item) {
+            super(item);
+            this.item = item;
+        }
+        public void setText(String text) {
+            TextView textView = item.findViewById(R.id.item_name);
+            textView.setText(text);
+        }
+    }
+
+    public MainListAdapter(List<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_list_item, null);
+        ViewHolder holder = new ViewHolder(item);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ViewHolder)holder).setText(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
