193c8b5cca75eec691855f30e3cfb2579f53c05c
Victor <vlopmartin@gmail.com>
2018-08-19 16:05:34 +0200

    Refactoring Task

--- app/src/main/java/com/vlopmartin/apps/organizer/Task.java
+++ app/src/main/java/com/vlopmartin/apps/organizer/Task.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import org.threeten.bp.LocalDate;
+import org.threeten.bp.Period;
 import org.threeten.bp.temporal.ChronoField;
 
 import java.util.ArrayList;
@@ -16,58 +17,63 @@ import java.util.List;
 
 public class Task {
 
-    public static final String createTableSQL = "CREATE TABLE TASKS (ID INTEGER PRIMARY KEY, NAME TEXT, DESCRIPTION TEXT, DUE_DATE INTEGER, PRIORITY INTEGER)";
+    public static final String createTableSQL = "CREATE TABLE TASKS (" +
+            "ID INTEGER PRIMARY KEY, " +
+            "NAME TEXT, " +
+            "DESCRIPTION TEXT, " +
+            "DUE_DATE INTEGER, " +
+            "PRIORITY INTEGER, " +
+            "REPEAT_PERIOD TEXT)";
 
     private long id;
-    private String name;
-    private String description;
-    private LocalDate dueDate;
-    private int priority;
-
-
-    public static List<Task> getList(Context ctx) {
-        List<Task> ret = new ArrayList<Task>();
-
-        SQLiteDatabase db = new DBHelper(ctx).getReadableDatabase();
-        Cursor cursor = db.query("TASKS", null, null, null, null, null, null);
-
-        long taskId;
-        String taskName;
-        String taskDescription;
-        long taskDueDate;
-        int taskPriority;
-        while (cursor.moveToNext()) {
-            taskId = cursor.getLong(cursor.getColumnIndex("ID"));
-            taskName = cursor.getString(cursor.getColumnIndex("NAME"));
-            taskDescription = cursor.getString(cursor.getColumnIndex("DESCRIPTION"));
-            taskDueDate = cursor.getLong(cursor.getColumnIndex("DUE_DATE"));
-            taskPriority = cursor.getInt(cursor.getColumnIndex("PRIORITY"));
-            ret.add(new Task(taskId, taskName, taskDescription, taskDueDate == 0 ? null : LocalDate.ofEpochDay(taskDueDate), taskPriority));
-        }
-
-        cursor.close();
-        return ret;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public static Task getById(Context ctx, long id) {
-        Task ret;
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        SQLiteDatabase db = new DBHelper(ctx).getReadableDatabase();
-        Cursor cursor = db.query("TASKS", null, "id = ?", new String[] {String.valueOf(id)}, null, null, null);
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        if (cursor.moveToNext()) {
-            long taskId = cursor.getLong(cursor.getColumnIndex("ID"));
-            String taskName = cursor.getString(cursor.getColumnIndex("NAME"));
-            String taskDescription = cursor.getString(cursor.getColumnIndex("DESCRIPTION"));
-            long taskDueDate = cursor.getLong(cursor.getColumnIndex("DUE_DATE"));
-            int taskPriority = cursor.getInt(cursor.getColumnIndex("PRIORITY"));
-            ret = new Task(taskId, taskName, taskDescription, taskDueDate == 0 ? null : LocalDate.ofEpochDay(taskDueDate), taskPriority);
-        } else {
-            ret = null;
-        }
+    private LocalDate dueDate;
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate; }
 
-        cursor.close();
-        return ret;
+    private int priority;
+    public int getPriority() { return priority; }
+    public void setPriority(int priority) { this.priority = priority; }
+
+    private Period repeatPeriod;
+    public Period getRepeatPeriod() { return repeatPeriod; }
+    public void setRepeatPeriod(Period repeatPeriod) { this.repeatPeriod = repeatPeriod; }
+
+    public Task(long id, String name, String description, LocalDate dueDate, int priority, Period repeatPeriod) {
+        setId(id);
+        setName(name);
+        setDescription(description);
+        setDueDate(dueDate);
+        setPriority(priority);
+        setRepeatPeriod(repeatPeriod);
+    }
+
+    public Task copy() {
+        return new Task(this.getId(), this.getName(), this.getDescription(), this.getDueDate(), this.getPriority(), this.getRepeatPeriod());
     }
 
     public void save(Context ctx) {
@@ -81,6 +87,7 @@ public class Task {
         values.put("DESCRIPTION", this.description);
         values.put("DUE_DATE", this.dueDate == null ? 0 : this.dueDate.getLong(ChronoField.EPOCH_DAY));
         values.put("PRIORITY", this.priority);
+        values.put("REPEAT_PERIOD", this.repeatPeriod == null ? "" : this.repeatPeriod.toString());
 
         long id = db.replace("TASKS", null, values);
         this.id = id;
@@ -92,13 +99,52 @@ public class Task {
         db.delete("TASKS", "ID = ?", new String[] {String.valueOf(this.id)});
     }
 
-    public Task(long id, String name, String description, LocalDate dueDate, int priority) {
-        setId(id);
-        setName(name);
-        setDescription(description);
-        setDueDate(dueDate);
-        setPriority(priority);
+    private static Task readCursor(Cursor cursor) {
+        long taskId = cursor.getLong(cursor.getColumnIndex("ID"));
+        String taskName = cursor.getString(cursor.getColumnIndex("NAME"));
+        String taskDescription = cursor.getString(cursor.getColumnIndex("DESCRIPTION"));
+        long taskDueDate = cursor.getLong(cursor.getColumnIndex("DUE_DATE"));
+        int taskPriority = cursor.getInt(cursor.getColumnIndex("PRIORITY"));
+        String taskRepeatPeriod = cursor.getString(cursor.getColumnIndex("REPEAT_PERIOD"));
+        return new Task(
+                taskId,
+                taskName,
+                taskDescription,
+                taskDueDate == 0 ? null : LocalDate.ofEpochDay(taskDueDate),
+                taskPriority,
+                Period.parse(taskRepeatPeriod));
     }
+
+    public static Task getById(Context ctx, long id) {
+        Task ret;
+
+        SQLiteDatabase db = new DBHelper(ctx).getReadableDatabase();
+        Cursor cursor = db.query("TASKS", null, "id = ?", new String[] {String.valueOf(id)}, null, null, null);
+
+        if (cursor.moveToNext()) {
+            ret = readCursor(cursor);
+        } else {
+            ret = null;
+        }
+
+        cursor.close();
+        return ret;
+    }
+
+    public static List<Task> getList(Context ctx) {
+        List<Task> ret = new ArrayList<Task>();
+
+        SQLiteDatabase db = new DBHelper(ctx).getReadableDatabase();
+        Cursor cursor = db.query("TASKS", null, null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            ret.add(readCursor(cursor));
+        }
+
+        cursor.close();
+        return ret;
+    }
+
 
     public static class PriorityComparator extends RankComparator<Task> {
 
@@ -134,38 +180,4 @@ public class Task {
         }
 
     }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate; }
-
-    public int getPriority() { return priority; }
-
-    public void setPriority(int priority) { this.priority = priority; }
 }
