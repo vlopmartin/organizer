24741e832a43ded1dd9c2821236f61cfd2d78e69
Victor <vlopmartin@gmail.com>
2018-08-31 22:40:09 +0200

    Added vibration and sound on notification

--- app/src/main/java/com/vlopmartin/apps/organizer/NotificationHelper.java
+++ app/src/main/java/com/vlopmartin/apps/organizer/NotificationHelper.java
@@ -6,8 +6,12 @@ import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.Builder;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationManagerCompat;
@@ -28,6 +32,7 @@ public abstract class NotificationHelper {
             String description = context.getString(R.string.notification_channel_description);
             NotificationChannel channel = new NotificationChannel(id, name, importance);
             channel.setDescription(description);
+            channel.setImportance(NotificationManager.IMPORTANCE_HIGH);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
@@ -40,14 +45,23 @@ public abstract class NotificationHelper {
         Builder builder = new Builder(context, channelId)
                 .setContentTitle(task.getName())
                 .setContentText(task.getDescription())
-                .setSmallIcon(R.drawable.ic_alarm_black_24dp);
+                .setSmallIcon(R.drawable.ic_alarm_black_24dp)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         Intent intent = new Intent(context, CompleteTaskReceiver.class);
         intent.putExtra(TaskDetailsActivity.TASK_ID, task.getId());
-        //Bundle extras = new Bundle();
-        //extras.putLong(TaskDetailsActivity.TASK_ID, task.getId());
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, CompleteTaskReceiver.REQUEST, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.addAction(R.drawable.ic_check_black_24dp, "Complete", pendingIntent);
+        builder.addAction(R.drawable.ic_check_black_24dp, context.getString(R.string.complete_button), pendingIntent);
+
+        int[] vibration_pattern_int = context.getResources().getIntArray(R.array.vibration_pattern);
+        long[] vibration_pattern = new long[vibration_pattern_int.length];
+        for (int i = 0; i < vibration_pattern_int.length; i++) {
+            vibration_pattern[i] = vibration_pattern_int[i];
+        }
+        builder.setVibrate(vibration_pattern);
+
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(sound);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify((int)task.getId(), builder.build());
